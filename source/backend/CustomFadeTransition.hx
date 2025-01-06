1f386874c0496df7db9a9fed9a79f01d9ab98caf
LarryFrosty <larrydobefrosty@gmail.com>
2025-01-06 23:15:07 +0800

    idk math i dont expect this to work

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -5,9 +5,10 @@ import flixel.FlxSubState;
 
 class CustomFadeTransition extends FlxSubState {
 	public static var finishCallback:Void->Void;
+	static var transitionImages:Array<String> = ['markiplier'];
+	static var image:String;
 	var isTransIn:Bool = false;
-	var transBlack:FlxSprite;
-	var transGradient:FlxSprite;
+	var transSprite:FlxSprite;
 
 	var duration:Float;
 	public function new(duration:Float, isTransIn:Bool)
@@ -19,27 +20,21 @@ class CustomFadeTransition extends FlxSubState {
 
 	override function create()
 	{
+		if (!isTransIn)
+			image = transitionImages[FlxG.random.int(0, transitionImages.length - 1, [transitionImages.indexOf(image)]);
+
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length-1]];
 		var width:Int = Std.int(FlxG.width / Math.max(camera.zoom, 0.001));
 		var height:Int = Std.int(FlxG.height / Math.max(camera.zoom, 0.001));
-		transGradient = FlxGradient.createGradientFlxSprite(1, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		transGradient.scale.x = width;
-		transGradient.updateHitbox();
-		transGradient.scrollFactor.set();
-		transGradient.screenCenter(X);
-		add(transGradient);
+		transSprite = new FlxSprite(Paths.image('transition/$image'));
+		transSprite.setGraphicSize(width, height);
+		transSprite.updateHitbox();
+		transSprite.scrollFactor.set();
+		transSprite.screenCenter(X);
+		add(transSprite);
 
-		transBlack = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
-		transBlack.scale.set(width, height + 400);
-		transBlack.updateHitbox();
-		transBlack.scrollFactor.set();
-		transBlack.screenCenter(X);
-		add(transBlack);
-
-		if(isTransIn)
-			transGradient.y = transBlack.y - transBlack.height;
-		else
-			transGradient.y = -transGradient.height;
+		if(!isTransIn)
+			transSprite.y = -transSprite.height;
 
 		super.create();
 	}
@@ -48,18 +43,13 @@ class CustomFadeTransition extends FlxSubState {
 		super.update(elapsed);
 
 		final height:Float = FlxG.height * Math.max(camera.zoom, 0.001);
-		final targetPos:Float = transGradient.height + 50 * Math.max(camera.zoom, 0.001);
+		final targetPos:Float = transSprite.height + 50 * Math.max(camera.zoom, 0.001);
 		if(duration > 0)
-			transGradient.y += (height + targetPos) * elapsed / duration;
+			transSprite.y += (height + targetPos) * elapsed / duration;
 		else
-			transGradient.y = (targetPos) * elapsed;
+			transSprite.y = (targetPos) * elapsed;
 
-		if(isTransIn)
-			transBlack.y = transGradient.y + transGradient.height;
-		else
-			transBlack.y = transGradient.y - transBlack.height;
-
-		if(transGradient.y >= targetPos)
+		if(transSprite.y >= targetPos)
 		{
 			close();
 		}
