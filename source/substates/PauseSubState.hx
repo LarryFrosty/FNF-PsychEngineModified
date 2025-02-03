3198b57fd8e52ee312f95b84727a5d3bf619d9af
LarryFrosty <larrydobefrosty@gmail.com>
2025-02-03 09:53:17 +0800

    fuck im so bad at math

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -198,13 +198,13 @@ class PauseSubState extends MusicBeatSubstate
 				if (controls.UI_LEFT_P)
 				{
 					FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-					curTime -= 1000;
+					curTime -= 1000 * PlayState.instance.playbackRate;
 					holdTime = 0;
 				}
 				if (controls.UI_RIGHT_P)
 				{
 					FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-					curTime += 1000;
+					curTime += 1000 * PlayState.instance.playbackRate;
 					holdTime = 0;
 				}
 
@@ -213,11 +213,11 @@ class PauseSubState extends MusicBeatSubstate
 					holdTime += elapsed;
 					if(holdTime > 0.5)
 					{
-						curTime += 45000 * elapsed * (controls.UI_LEFT ? -1 : 1);
+						curTime += 45000 * elapsed * (controls.UI_LEFT ? -1 : 1) * PlayState.instance.playbackRate;
 					}
 
-					if(curTime >= FlxG.sound.music.length) curTime -= FlxG.sound.music.length;
-					else if(curTime < 0) curTime += FlxG.sound.music.length;
+					if(curTime >= FlxG.sound.music.length) curTime -= FlxG.sound.music.length / PlayState.instance.playbackRate;
+					else if(curTime < 0) curTime += FlxG.sound.music.length / PlayState.instance.playbackRate;
 					updateSkipTimeText();
 				}
 		}
@@ -287,15 +287,15 @@ class PauseSubState extends MusicBeatSubstate
 				case 'Skip Time':
 					if(curTime < Conductor.songPosition)
 					{
-						PlayState.startOnTime = curTime * PlayState.instance.playbackRate;
+						PlayState.startOnTime = curTime;
 						restartSong(true);
 					}
 					else
 					{
 						if (curTime != Conductor.songPosition)
 						{
-							PlayState.instance.clearNotesBefore(curTime * PlayState.instance.playbackRate);
-							PlayState.instance.setSongTime(curTime * PlayState.instance.playbackRate);
+							PlayState.instance.clearNotesBefore(curTime);
+							PlayState.instance.setSongTime(curTime);
 						}
 						close();
 					}
