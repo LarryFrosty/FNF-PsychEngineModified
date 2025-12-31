99f2f0633bf7eee72edb968f79699595fb07cf5f
LarryFrosty <larrydobefrosty@gmail.com>
2025-12-31 19:17:39 +0800

    how tf cant you find null

--- source/AssetPaths.hx
+++ source/AssetPaths.hx
@@ -31,7 +31,7 @@ package;
  * and provides you some control on how `AssetPaths`'s fields are built.
  * @see [Flixel 5.0.0 Migration guide - AssetPaths has less caveats](https://github.com/HaxeFlixel/flixel/wiki/Flixel-5.0.0-Migration-guide#assetpaths-has-less-caveats-2575)
 **/
-@:build(flixel.system.FlxAssets.buildFileReferences("assets", true, null, null, function(name:String):Null<String>
+@:build(flixel.system.FlxAssets.buildFileReferences("assets", true, null, null, function(name:String)
 	{
 		return name.toLowerCase()
 			.split("/").join("_")
