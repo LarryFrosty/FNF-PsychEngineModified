af0d170caf323cdebaebe3d9c292b3bcec409dd8
LarryFrosty <larrydobefrosty@gmail.com>
2026-01-21 14:15:13 +0800

    TextField Debugger (in preparation for global scripts)

---
+++ source/debug/DebugText.hx
@@ -0,0 +1,32 @@
+package debug;
+
+import openfl.text.TextField;
+import openfl.text.TextFornat;
+import openfl.utils.Assets;
+
+class DebugText extends TextField
+{
+	public var disableTime:Float = 6;
+	public function new(text:String, color:Int) {
+		super();
+		defaultTextFormat = new TextFormat(Assets.getFont(Paths.font('vcr.ttf')).fontName, 28, color);
+		width = FlxG.width - 20;
+		wordWrap = true;
+		multiline = true;
+		selectable = false;
+		embedFonts = true;
+		sharpness = 100;
+		autoSize = LEFT;
+		text = text;
+		scaleX = scaleY = 1;
+		x = FlxG.game.x + 10;
+		y = FlxG.game.y + 10;
+	}
+
+	override function __enterFrame(deltaTime:Float) {
+		disableTime -= deltaTime;
+		if(disableTime < 0) disableTime = 0;
+		if(disableTime < 1) alpha = disableTime;
+		super.__enterFrame(deltaTime);
+	}
+}
