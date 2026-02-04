073c333bccee500bf21392677d030bdba516533a
LarryFrosty <larrydobefrosty@gmail.com>
2026-02-04 17:55:07 +0800

    add global map abstract from sscript
    makes it so that if the values of a global var changes then all the other scripts will actually have its updated value

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -1,5 +1,13 @@
 package psychlua;
 
+import haxe.ds.StringMap;
+import haxe.ds.IntMap;
+import haxe.ds.ObjectMap;
+import haxe.ds.EnumValueMap;
+import haxe.Constraints.IMap;
+
+import haxe.ValueException;
+
 import flixel.FlxBasic;
 import objects.Character;
 import psychlua.LuaUtils;
@@ -17,7 +25,7 @@ import crowplexus.hscript.Expr.Error as IrisError;
 import crowplexus.hscript.Tools;
 import crowplexus.hscript.Printer;
 
-import haxe.ValueException;
+typedef HScriptGlobalMap = HScriptTypedGlobalMap<String, Dynamic>;
 
 typedef HScriptInfos = {
 	> haxe.PosInfos,
@@ -30,7 +38,7 @@ typedef HScriptInfos = {
 
 class HScript extends Iris
 {
-	public static var globalVariables:Map<String, Dynamic> = new Map();
+	public static var globalVariables:HScriptGlobalMap = new HScriptGlobalMap();
 
 	public var filePath:String;
 	public var modFolder:String;
@@ -639,6 +647,88 @@ class CustomInterp extends crowplexus.hscript.Interp
 		}
 		return v;
 	}
+}
+
+// from SScript
+@:transitive
+@:multiType(@:followWithAbstracts K)
+abstract HScriptTypedGlobalMap<K, V>(IMap<K, V>) 
+{
+	public function new();
+
+	public inline function set(key:K, value:V)
+	{
+		this.set(key, value);
+
+		var key:String = cast key;
+		var value:Dynamic = cast value;
+		for (i in Iris.instances)
+		{
+			if (!i.interp != null)
+				i.set(key, value);
+		}
+	}
+
+	@:arrayAccess public inline function get(key:K)
+		return this.get(key);
+
+	public inline function exists(key:K)
+		return this.exists(key);
+
+	public inline function remove(key:K)
+		return this.remove(key);
+
+	public inline function keys():Iterator<K> 
+		return this.keys();
+
+	public inline function iterator():Iterator<V> 
+		return this.iterator();
+
+	public inline function keyValueIterator():KeyValueIterator<K, V> 
+		return this.keyValueIterator();
+
+	public inline function copy():Map<K, V> 
+		return cast this.copy();
+
+	public inline function toString():String 
+		return this.toString();
+
+	public inline function clear():Void 
+		this.clear();
+
+	@:arrayAccess @:noCompletion public inline function arrayWrite(k:K, v:V):V 
+	{
+		this.set(k, v);
+		var key:String = cast k;
+		var value:Dynamic = cast v;
+		for (i in Iris.instances)
+		{
+			if (!i.interp != null)
+				i.set(key, value);
+		}
+		return v;
+	}
+
+	@:to static inline function toStringMap<K:String, V>(t:IMap<K, V>):StringMap<V> 
+		return new StringMap<V>();
+
+	@:to static inline function toIntMap<K:Int, V>(t:IMap<K, V>):IntMap<V> 
+		return new IntMap<V>();
+
+	@:to static inline function toEnumValueMapMap<K:EnumValue, V>(t:IMap<K, V>):EnumValueMap<K, V> 
+		return new EnumValueMap<K, V>();
+
+	@:to static inline function toObjectMap<K:{}, V>(t:IMap<K, V>):ObjectMap<K, V> 
+		return new ObjectMap<K, V>();
+
+	@:from static inline function fromStringMap<V>(map:StringMap<V>):HScriptTypedGlobalMap<String, V> 
+		return cast map;
+
+	@:from static inline function fromIntMap<V>(map:IntMap<V>):HScriptTypedGlobalMap<Int, V> 
+		return cast map;
+
+	@:from static inline function fromObjectMap<K:{}, V>(map:ObjectMap<K, V>):HScriptTypedGlobalMap<K, V> 
+		return cast map;
 }
 #else
 class HScript
